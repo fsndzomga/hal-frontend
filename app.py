8c3408d207a2a467bb7e1b614ef7e1fd0cb4a5e4
benediktstroebl <stroebl@princeton.edu>
2024-12-13 19:19:12 -0500

    update

--- app.py
+++ app.py
@@ -206,8 +206,7 @@ def create_app():
     return app
 
 
+app = create_app()
+
 if __name__ == '__main__':
-    preprocessor = TracePreprocessor()
-    preprocessor.preprocess_traces()
-    app = create_app()
     app.run(debug=False, port=5001, host='0.0.0.0')
