30b83fd4cc817db3c217c56da129c70509b6ae7b
Franck Stéphane Ndzomga <101533724+fsndzomga@users.noreply.github.com>
2025-10-01 14:53:53 +0000

    use env port for render

--- app.py
+++ app.py
@@ -1067,4 +1067,6 @@ def create_app():
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=False, port=5001, host='0.0.0.0')
+    import os
+    port = int(os.environ.get('PORT', 5001))
+    app.run(debug=False, port=port, host='0.0.0.0')
