fa8a6a43efdca73c31e2931778d6e15284afb5d4
Franck Stéphane Ndzomga <101533724+fsndzomga@users.noreply.github.com>
2025-10-14 12:22:52 +0000

    port issues

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn -c gunicorn.conf.py app:app
+web: gunicorn app:app --bind 0.0.0.0:$PORT
--- gunicorn.conf.py
+++
@@ -1,8 +0,0 @@
-import os
-
-bind = f"0.0.0.0:{os.environ.get('PORT', '10000')}"
-workers = 1
-timeout = 120
-keepalive = 2
-max_requests = 1000
-max_requests_jitter = 100
